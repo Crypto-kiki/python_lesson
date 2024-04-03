d885bdeaa7f79686456d153c5e25e289bb21e662
baekki <baekki@baekkiui-MacBookPro.local>
2024-04-04 02:20:49 +0900

    blog details

--- src/components/Blog/BlogDetails.js
+++ src/components/Blog/BlogDetails.js
@@ -1,15 +1,12 @@
-// src/components/Blog/BlogDetails.js
-
 import { format, parseISO } from "date-fns";
-import { slug } from "github-slugger";
 import Link from "next/link";
 import React from "react";
+import { slug } from "github-slugger";
 import ViewCounter from "./ViewCounter";
 
 const BlogDetails = ({ blog, slug: blogSlug }) => {
-  console.log(blog);
   return (
-    <div className="px-2 md:px-10 bg-accent dark:bg-accentDark text-light dark:text-dark py-2 flex items-center justify-around flex-wrap text-lg sm:text-xl font-medium mx-5 md:mx-10 rounded-lg">
+    <div className="px-2  md:px-10 bg-accent dark:bg-accentDark text-light dark:text-dark py-2 flex items-center justify-around flex-wrap text-lg sm:text-xl font-medium mx-5  md:mx-10 rounded-lg">
       <time className="m-3">
         {format(parseISO(blog.publishedAt), "LLLL d, yyyy")}
       </time>
@@ -17,7 +14,7 @@ const BlogDetails = ({ blog, slug: blogSlug }) => {
         <ViewCounter slug={blogSlug} />
       </span>
       <div className="m-3">{blog.readingTime.text}</div>
-      <Link href={`categories/${slug(blog.tags[0])}`} className="m-3">
+      <Link href={`/categories/${slug(blog.tags[0])}`} className="m-3">
         #{blog.tags[0]}
       </Link>
     </div>
