828baf73d385ddc8d219af23ed5cb4a83a10fa15
hinggu <110807116+Crypto-kiki@users.noreply.github.com>
2026-03-10 08:31:25 +0800

    Fix tag visibility across category and blog pages

--- src/components/Home/HomeCoverSection.js
+++ src/components/Home/HomeCoverSection.js
@@ -27,7 +27,11 @@ const HomeCoverSection = ({ blogs }) => {
         />
 
         <div className="w-full lg:w-3/4 p-4 sm:p-6 md:p-8 lg:p-10 flex flex-col items-start justify-center z-0 text-light">
-          <Tag link={`/categories/${slug(blog.tags[0])}`} name={blog.tags[0]} className="mt-2" />
+          <div className="mt-2 flex flex-wrap gap-2">
+            {blog.tags.map((tag) => (
+              <Tag key={tag} link={`/categories/${slug(tag)}`} name={tag} />
+            ))}
+          </div>
           <Link href={blog.url} className="mt-3">
             <h1 className="font-bold capitalize text-base sm:text-xl md:text-2xl lg:text-3xl leading-tight">
               <span className="bg-gradient-to-r from-accentDark/70 to-accentDark/70 bg-[length:0px_6px] hover:bg-[length:100%_6px] bg-left-bottom bg-no-repeat transition-[background-size] duration-700">
