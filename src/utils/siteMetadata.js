32aa8fda28972e9e3e6544a98a14af7160e89056
Crypto-kiki <ejo2123@naver.com>
2026-03-16 17:23:58 +0800

    siteMetadata

--- src/utils/siteMetadata.js
+++ src/utils/siteMetadata.js
@@ -8,10 +8,10 @@ const siteMetadata = {
     "실무형 파이썬 강의 교안과 실습 중심의 학습 자료를 제공하는 Python Lesson 웹사이트입니다.",
   language: "ko-kr",
   theme: "system",
-  siteUrl: "https://python-lesson-lab.vercel.app",
+  siteUrl: "https://python-lesson-red.vercel.app/",
   siteLogo: "/profile.png",
   socialBanner: "/social-banner.png",
-  email: "hello@pythonlessonlab.dev",
+  email: "kbg20100106@gmail.com",
   locale: "ko-kr",
 };
 
